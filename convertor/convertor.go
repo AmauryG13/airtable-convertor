303e1168afd8ebace70129d29b4a2672adcdadee
Amaury Guillermin <amauryguillermin@gmail.com>
2020-06-11 21:12:48 +0200

    Initialization logic refactored

--- convertor/convertor.go
+++ convertor/convertor.go
@@ -1,51 +1,68 @@
 package convertor
 
 import (
+	"log"
+	"os"
 	"path/filepath"
-
-	"github.com/amauryg13/airtor/convertor/formatter"
-	"github.com/amauryg13/airtor/convertor/parser"
-	"github.com/amauryg13/airtor/convertor/reader"
-	"github.com/amauryg13/airtor/convertor/writer"
 )
 
 // Convertor is the main struct
 type Convertor struct {
-	Input  string
-	Output string
-	Reader *reader.Reader
-	Parser *parser.Parser
-	Format *formatter.Formatter
-	Writer *writer.Writer
+	Input    string
+	Output   string
+	Info     *Info
+	Channels map[string]chan []string
+}
+
+type Info struct {
+	Dir       string
+	Filename  string
+	Extension string
+}
+
+func newInfo(dir string, name string, ext string) *Info {
+	return &Info{
+		Dir:       dir,
+		Filename:  name,
+		Extension: ext,
+	}
+}
+
+func newChannels() map[string]chan []string {
+	var Channels map[string]chan []string
+
+	Channels["read"] = make(chan []string)
+	Channels["write"] = make(chan []string)
+
+	return Channels
 }
 
 // NewConvertor is creating a new convertor
 func NewConvertor(filePath string, addChars []string, options map[string]string) *Convertor {
 	dir, file := filepath.Split(filePath)
+
+	if dir == "" {
+		cwd, cwdErr := os.Getwd()
+		dir = cwd
+
+		if cwdErr == nil {
+			log.Fatal(cwdErr)
+		}
+	}
+
 	ext := filepath.Ext(file)
+
+	cInfo := newInfo(dir, file, ext)
+
+	iFilePath := filepath.Join(dir, file)
 
 	oFileName := file[:len(file)-len(ext)] + "_" + addOnOutToken + ext
 	oFilePath := filepath.Join(dir, oFileName)
 
 	return &Convertor{
-		Input:  filePath,
-		Output: oFilePath,
-		Reader: reader.NewReader(filePath),
-		Parser: parser.NewParser(addChars),
-		Format: formatter.NewFormatter(ext, options),
-		Writer: writer.NewWriter(oFilePath, options),
-	}
-}
-
-// Run is running the conversion logic
-func (c *Convertor) Run() {
-	buffer := c.Reader.Read()
-
-	content := c.Format.Read(buffer)
-
-	updatedContent := c.Parser.Parse(content)
-
-	for _, record := range updatedContent {
-		c.Writer.Write(record)
+		Input:    iFilePath,
+		Output:   oFilePath,
+		Info:     cInfo,
+		Channels: newChannels(),
 	}
 }
